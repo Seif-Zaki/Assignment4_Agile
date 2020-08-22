d307c68af2f7310058b82a8cba14a067b3149795
Seif-Zaki <seiouf97@hotmail.com>
2020-08-22 08:16:18 +0200

    operations function

---
+++ operations.py
@@ -0,0 +1,9 @@
+def calculate(n1, op, n2):
+    if op == '+':
+        return n1 + n2
+    elif op == '-':
+        return n1 - n2
+    elif op == '*':
+        return n1*n2
+    elif op == '/':
+        return n1/n2
